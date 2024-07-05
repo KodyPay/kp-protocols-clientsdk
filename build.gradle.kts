1e81f51eca484ba7c05acbe0a8e3d16ab05b6a01
Andrea Fabris <97245545+afabris-kodypay@users.noreply.github.com>
2024-07-05 15:12:20 +0100

    feat: add gradle files to check proto syntax (#19)

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import com.google.protobuf.gradle.*
+
+plugins {
+    id("java")
+    id("idea")
+    id("com.google.protobuf") version "0.9.4"
+}
+
+repositories {
+    mavenCentral()
+}
+
+val protbufVersion = "4.27.2"
+val grpcVersion = "1.65.0"
+val javaxAnnotationsVersion = "1.3.2"
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-java:$protbufVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("javax.annotation:javax.annotation-api:$javaxAnnotationsVersion")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:$protbufVersion"
+    }
+    plugins {
+        id("grpc") { artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion" }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+               id("grpc") { }
+            }
+        }
+    }
+}
